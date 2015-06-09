87cb61988bf0ddb9be209055a7ad7b6f1d6ce144
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-06-09 14:57:49 -0600

    Tiny readability refactor

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -2,6 +2,6 @@ Spree::Variant.class_eval do
   attr_accessible :discontinued_at
 
   def discontinued?
-    !discontinued_at.nil? && discontinued_at < DateTime.now
+    discontinued_at && discontinued_at < DateTime.now
   end
 end
