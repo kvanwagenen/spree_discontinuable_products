53473440de4fc84061193099156006d7ea96cd01
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-06-10 14:34:28 -0600

    Add hook after discontinued_at is set

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -2,4 +2,13 @@ Spree::Variant.class_eval do
   def discontinued?
     discontinued_at && discontinued_at < DateTime.now
   end
+
+  def discontinued_at=(discontinued_at)
+    write_attribute(:discontinued_at, discontinued_at)
+    after_set_discontinued_at
+  end
+
+  def after_set_discontinued_at
+    # To be overridden
+  end
 end
